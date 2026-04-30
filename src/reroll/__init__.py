4cb589651717937ae3c05a9af0582163acde3a47
Jeffrey Newman <jeff@driftless.xyz>
2026-04-30 09:06:56 -0500

    add dependencies, imports

--- src/reroll/__init__.py
+++ src/reroll/__init__.py
@@ -1,5 +1,6 @@
 __all__ = (  # noqa: F405
     # TODO: Add all public symbols here.
+    "FastChannel",
 )
 
-from .reroll import *  # noqa: F403
+from ._fast_channel import FastChannel  # noqa: F403
